4e2c4d990c5c5e2041f0c1c6fe51d8b45eefd940
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-02-18 09:32:01 +0800

    Create Inheritance.java

---
+++ MOOP/Inheritance.java
@@ -0,0 +1,20 @@
+package Demo;
+import java.util.*;
+
+class Testing{
+    public void sayHi(){
+        System.out.println("I'm the super class");
+    }
+}
+
+public class Demo extends Testing{
+    public void sayHiToo(){
+        super.sayHi();      //accessing testing as the super class
+        System.out.println("Hi, from the other side");
+    }
+
+    public static void main(String[] args) {
+        Demo coba = new Demo();
+        coba.sayHiToo();
+    }
+}
