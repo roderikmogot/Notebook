245c48340c64937e3b20538b3a63d235c84750d8
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-02-19 22:18:10 +0800

    Update Fastscanner.java

--- MOOP/Fastscanner.java
+++ MOOP/Fastscanner.java
@@ -1,4 +1,11 @@
 //src: SecondThread
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
 static class FastScanner {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer("");
