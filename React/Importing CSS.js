000eb26b74e3609714cc8db7ad7997d403d3bcff
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-03-03 20:51:08 +0800

    Create Importing CSS.js

---
+++ React/Importing CSS.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+};
+
+export default person;
+
+// CSS
+.Person{
+    width: 60%;
+    margin: auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+}
