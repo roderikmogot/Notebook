d76d961c522412de71db2cb2763aacc03130c583
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-03-03 20:59:06 +0800

    Create Inline CSS.js

---
+++ React/Inline CSS.js
@@ -0,0 +1,74 @@
+import './App.css';
+import React, {Component} from 'react';
+import Person from './Person/Person.js';
+
+class App extends Component{
+    state = {
+        persons: [
+            {name: 'Rod', age: 17},
+            {name: 'Randy', age: 16},
+            {name: 'Will', age: 15}
+        ],
+        anotherVar: 'hello'
+    };
+
+    switchNameHandler = (newName) => {
+            // DON'T DO THIS: this.state.persons[0].name = 'Mogot';
+            this.setState({
+                persons: [
+                    {name: newName, age: 27},
+                    {name: 'Randy', age: 16},
+                    {name: 'Will', age: 35}
+                ]
+            });
+        };
+
+    nameChangedHandler = event => {
+        this.setState({
+            persons: [
+                {name: 'Rod', age: 27},
+
+                // Target -> the input in which we typed
+                // Value -> what we typed
+                {name: event.target.value, age: 16},
+
+                {name: 'Will', age: 35}
+            ]
+        });
+    }
+
+    render() {
+
+        // Inline Javascript CSS
+        const styleCSS = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
+        return (
+            <div className="App">
+                <h1>Hi, I'm React App</h1>
+                <button style={styleCSS} onClick={() => this.switchNameHandler('Roderik')}>Switch name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Rode')}
+                    changed={this.nameChangedHandler}
+                    >
+                    My hobbies: running </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+            </div>
+        )
+    }
+}
+
+export default App;
